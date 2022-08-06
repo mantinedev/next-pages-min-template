5800794e1db1e9e77b1d969c1e8c88a5618e9f8b
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-06 11:09:20 +0300

    Init template

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button, Group } from "@mantine/core";
+
+export default function IndexPage() {
+  return (
+    <Group mt={50} position="center">
+      <Button size="xl">Welcome to Mantine!</Button>
+    </Group>
+  );
+}
