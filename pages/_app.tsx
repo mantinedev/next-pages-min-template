09c8fe06bdbac99a0065282c8da371ab006816c1
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-14 13:28:12 +0400

    Add theme and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import "@mantine/core/styles.css";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { theme } from "../theme";
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Head>
         <title>Mantine Template</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
