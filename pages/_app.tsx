e8f502d1aa254524099b2f9ef2e2265d1109fe69
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-18 17:58:59 +0400

    Update template to 7.0 (#3)
    * Migrate to 7.0 alpha
    
    * Update readme
    
    * Update @mantine/* dependencies to version 1.6.0
    
    * Update @mantine/* dependencies to version 7.0.0-beta.2
    
    * Update @mantine/* dependencies to version 7.0.0-beta.5
    
    * Add theme and meta tags
    
    * Fix default color scheme not matching
    
    * Update @mantine/* dependencies to version 7.0.0-beta.7
    
    * Update @mantine/* dependencies to version 7.0.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,20 @@
-import { AppProps } from "next/app";
+import "@mantine/core/styles.css";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { theme } from "../theme";
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
-
+export default function App({ Component, pageProps }: any) {
   return (
-    <>
+    <MantineProvider theme={theme}>
       <Head>
-        <title>Page title</title>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <title>Mantine Template</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "dark",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
+      <Component {...pageProps} />
+    </MantineProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Button, Group } from "@mantine/core";
 
 export default function IndexPage() {
   return (
-    <Group mt={50} position="center">
+    <Group mt={50} justify="center">
       <Button size="xl">Welcome to Mantine!</Button>
     </Group>
   );
