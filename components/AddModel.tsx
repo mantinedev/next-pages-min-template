dafbe62d85065b9cea28a343f2a345ea7e4cdced
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-07-07 03:10:09 +0200

    file upload

--- components/AddModel.tsx
+++ components/AddModel.tsx
@@ -4,30 +4,47 @@ import { IconUpload } from '@tabler/icons-react';
 import { useApiAimodelsCreate } from '../api/endpoints/api/api';
 import { notifications } from '@mantine/notifications';
 import { ModelForm } from './ModelForm';
-
+import axios from 'axios';
+import { useState } from 'react';
 
 const AddModel = ({refetchParent, problems, types}) => {
   const [opened, { open, close }] = useDisclosure(false);
+  const [isPending, setIsPending] = useState(false);
   const {isPending: pendingCreation, mutateAsync: createAsync} = useApiAimodelsCreate();
   
   
 
-  const createProblem = async (payload: any) => {
-    createAsync({data: payload}).then(() => {
+  const createModel = async (payload: any) => {
+    console.log(payload);
+    const formData = new FormData();
+    formData.append('file', payload.file);
+    formData.append('name', payload.name);
+    formData.append('description', payload.description);
+    formData.append('problem', payload.problem);
+    formData.append('type', payload.type);
+    setIsPending(true);
+    axios.post('http://localhost:8000/api/aimodels/', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(() => {
         close();
         notifications.show({message: 'Model deployed successfully', color: 'green'});
         if (refetchParent) refetchParent();
     }).catch((error: any) => {
         notifications.show({message: `Model deployment failed: ${JSON.stringify(error.response?.data)}`, color: 'red'});
-    })
+    }).finally(() => {
+        setIsPending(false);
+    });
+
 };
 
 
   return (
     <>
       <Modal opened={opened} onClose={close} title="Deploy a model">
-        <LoadingOverlay visible={pendingCreation} zIndex={1000}/>
-        <ModelForm actionFunction={createProblem} />
+        <LoadingOverlay visible={isPending} zIndex={1000}/>
+        <ModelForm actionFunction={createModel} problems={problems} types={types} />
       </Modal>
       <Button onClick={open} justify="center"  radius={'xl'} rightSection={<IconUpload size={20}/>} style={{maxWidth: 300, margin: 'auto'}} variant="outline" mt="lg" mb='sm'>Deploy your own model</Button>
       </>
--- components/ModelForm.tsx
+++ components/ModelForm.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { TextInput, Checkbox, Button, Group, Box, FileInput, NativeSelect } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { File } from 'buffer';
 
-const ModelForm = ({actionFunction}) => {
+const ModelForm = ({actionFunction, problems, types}) => {
+
+    let empty_types = {id: 0, name: 'Select a type...'};
+    let empty_problems = {id: 0, name: 'Select a problem...'};
+    if (!types) types = empty_types;
+    if (!problems) problems = empty_problems;
+
+    let n_type = [empty_types, ...types];
+    let n_problem = [empty_problems, ...problems];
     
     const form = useForm({
-    validate: {
-      name: (value) => (value.trim() !== '' ? null : 'Name is required'),
-      description: (value) => (value.trim() !== '' ? null : 'Description is required'),
-      nevermind_tag: (value) => (value.trim() !== '' ? null : 'Tag is required'),
-    },
+        initialValues: {
+            name: '',
+            description: '',
+            problem: '',
+            type: '',
+            file: null
+        },
+        
   });
 
   return (
@@ -18,7 +30,7 @@ const ModelForm = ({actionFunction}) => {
         <TextInput
           withAsterisk
           label="Name"
-          placeholder="Problem of classification..."
+          placeholder="Uniswap Vault Manager..."
 
           {...form.getInputProps('name')}
         />
@@ -27,18 +39,43 @@ const ModelForm = ({actionFunction}) => {
           mt='xs'
           withAsterisk
           label="Description"
-          placeholder="Classify images into categories..."
+          placeholder="Manages stuff..."
           {...form.getInputProps('description')}
         />
 
-        <TextInput
-          mt='xs'
-          withAsterisk
-          label="URL"
-          placeholder="https://gateway.filecoin.io/..."
-          {...form.getInputProps('data_url')}
-        />
+        <NativeSelect
+            mt='xs'
+            label="Problem"
+            placeholder="Select a problem..."
+            required
+            {...form.getInputProps('problem')}
+        >
+            {n_problem.map((problem) => (
+                <option key={problem.id} value={problem.id}>{problem.name}</option>
+            ))}
+        </NativeSelect>
 
+        <NativeSelect
+            mt='xs'
+            label="Type"
+            placeholder="Select a type..."
+            required
+            
+            {...form.getInputProps('type')}
+        >
+            {n_type.map((type) => (
+                <option key={type.id} value={type.id}>{type.name}</option>
+            ))}
+        </NativeSelect>
+        <FileInput
+            mt='xs'
+            label="File in .onnx format"
+            accept=".onnx"
+            placeholder="Select & upload your model..."
+            required
+
+            {...form.getInputProps('file')}
+        />
 
         <Group position="right" mt="lg">
           <Button type="submit">Deploy model</Button>
