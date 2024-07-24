89915d1ee77df7380603d1bae126393fc4737626
bb-centrum <benbridges@centrumpile.co.uk>
2024-07-24 16:00:15 +0100

    ✨ (gitignore): add entries for macOS and Serwist service worker files to improve project cleanliness ✨ (next.config.mjs): integrate Serwist for service worker management to enhance PWA capabilities ⬆️ (package.json): add Serwist dependency to enable service worker functionality ✨ (public): add various favicon and icon files for better branding and user experience ♻️ (public): remove outdated favicon.svg to streamline asset management
    ✨ (safari-pinned-tab.svg): add Safari pinned tab SVG file to enhance browser support and user experience
    
    📝 (svg): update SVG path data to improve rendering and visual quality of the graphic
    
    ✨ (webmanifest, service-worker, tsconfig): add web manifest for PWA support and service worker for caching
    🔧 (tsconfig): update TypeScript configuration to include webworker lib and Serwist types for better type support

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
 
-export default nextConfig;
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "service-worker/index.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist({
+  reactStrictMode: true,
+});
+
